72ac64e7baebf296f6c287db36a73d323f2871f6
Alexey Kutepov <a.kutepov@voskhod.ru>
2017-04-04 11:26:16 +0300

    Initial commit

---
+++ app/src/main/java/wear/smart/ru/smartwear/MainActivity.java
@@ -0,0 +1,13 @@
+package wear.smart.ru.smartwear;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
